d0deb5028bbb9f0e6ecc055980e7e90e83f5d6da
Stephen Jue <stepjue@gmail.com>
2026-05-30 23:23:52 -0400

    more images, autogenerate set.html for each set

---
+++ sets.js
@@ -0,0 +1,17 @@
+const sets = {
+  "01": {
+    title: "marsh",
+    path: "images/set-01",
+    images: ["01.jpg", "02.jpg", "03.jpg"]
+  },
+  "02": {
+    title: "foss",
+    path: "images/set-02",
+    images: ["01.jpg", "04.jpg", "05.jpg", "02.jpg"]
+  },
+  "03": {
+    title: "geom",
+    path: "images/set-03",
+    images: ["01.jpg", "05.jpg", "04.jpg"]
+  }
+};
